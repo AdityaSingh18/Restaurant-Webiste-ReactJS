a3b12546688e61af05c99dfc474d8129544dbd26
Aditya <aditya18.mail@gmail.com>
2023-03-01 11:32:45 +0530

    Buidling the Header

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import HeaderRes from './HeaderRes'
+
+function App() {
+  return (
+    <div>
+      <HeaderRes/>
+      <h2>Let's get started!</h2>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/HeaderRes.js
@@ -0,0 +1,21 @@
+import React from "react";
+import './HeaderRes.css'
+
+const HeaderRes = ()=>{
+
+return (
+    <React.Fragment>
+        <h1>
+            <div className="header">
+            ReactMeals
+            <button>Your Cart</button>
+            
+            </div>
+            <img src="https://www.steriflow.com/wp-content/uploads/2020/12/propo-agro.jpg"></img>
+        </h1>
+    </React.Fragment>
+)
+}
+
+
+export default HeaderRes;
